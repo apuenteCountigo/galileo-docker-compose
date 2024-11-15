7c0f550502166ff8dbc74aeb9ee28895d42ff233
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-11-15 00:13:37 -0500

    INIT 1.1.3 ENTREGA A MOME

--- galileo-save-images.sh
+++ galileo-save-images.sh
@@ -13,9 +13,9 @@ while IFS= read -r IMAGE_NAME; do
     if [ -n "$IMAGE_NAME" ]; then
         # Eliminar el prefijo "galileo/" del nombre de la imagen
         CLEANED_NAME="${IMAGE_NAME#galileo/}"
-        OUTPUT_FILE="${DEST_DIR}/${CLEANED_NAME}_1.1.1.tar.gz"
-        echo "Guardando la imagen '$IMAGE_NAME':1.1.1 en '$OUTPUT_FILE'..."
-        docker save -o "$OUTPUT_FILE" "${IMAGE_NAME}:1.1.1"
+        OUTPUT_FILE="${DEST_DIR}/${CLEANED_NAME}_1.1.3.tar.gz"
+        echo "Guardando la imagen '$IMAGE_NAME':1.1.2 en '$OUTPUT_FILE'..."
+        docker save -o "$OUTPUT_FILE" "${IMAGE_NAME}:1.1.2"
     else
         echo "Línea vacía o nombre de imagen no válido, omitiendo..."
     fi
