b93be558f415740ed1639f5d8fafdad0f7d3374e
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-11-20 12:36:13 -0500

    galileo-docker-tag

---
+++ galileo-docker-tag-upgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Nombre del archivo que contiene los nombres de las imágenes
+IMAGES_FILE="imagenes.txt"
+# Carpeta de destino para guardar las imágenes
+DEST_DIR="/home/mome/galileo-images-v1.1"
+
+# Crear la carpeta de destino si no existe
+mkdir -p "$DEST_DIR"
+
+# Leer cada línea del archivo y ejecutar docker save para cada imagen
+while IFS= read -r IMAGE_NAME; do
+    if [ -n "$IMAGE_NAME" ]; then
+        # Eliminar el prefijo "galileo/" del nombre de la imagen
+        CLEANED_NAME="${IMAGE_NAME#galileo/}"
+        OUTPUT_FILE="${DEST_DIR}/${CLEANED_NAME}_1.1.3.tar.gz"
+        docker tag "${IMAGE_NAME}":1.1.2 "${IMAGE_NAME}":1.1.3
+        echo "Guardando la imagen '$IMAGE_NAME':1.1.2 en '$OUTPUT_FILE'..."
+        docker save -o "$OUTPUT_FILE" "${IMAGE_NAME}:1.1.3"
+    else
+        echo "Línea vacía o nombre de imagen no válido, omitiendo..."
+    fi
+done < "$IMAGES_FILE"
+
+echo "Todas las imágenes han sido guardadas en la carpeta '$DEST_DIR'."
