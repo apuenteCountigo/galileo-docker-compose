b33979b250398fdbf3d2df7ca71a38f1842b6235
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-11-20 12:54:41 -0500

    galileo-docker-rmi

---
+++ galileo-docker-rmi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Nombre del archivo que contiene los nombres de las imágenes
+IMAGES_FILE="imagenes.txt"
+
+# Leer cada línea del archivo y ejecutar docker save para cada imagen
+while IFS= read -r IMAGE_NAME; do
+    if [ -n "${$IMAGE_NAME}:1.1.2" ]; then
+        docker rmi -f "${IMAGE_NAME}":1.1.2
+    else
+        echo "Línea vacía o nombre de imagen no válido, omitiendo..."
+    fi
+done < "$IMAGES_FILE"
+
+echo "Todas las imágenes han sido eliminadas."
